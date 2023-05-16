69ab5a5edece6973b4fb4cb788439230ee4f755e
Grzegorz Grzęda <grzegorz.grzeda@g2labs.pl>
2023-05-17 00:06:25 +0200

    Fix CI build

--- document.sh
+++ document.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-$SCRIPT_DIR/packages/simple-documentation-library/document.py containers 1.2.1 "Simple containers library in C" source documentation
+$SCRIPT_DIR/packages/simple-documentation-library/document.py containers 1.2.2 "Simple containers library in C" source documentation
